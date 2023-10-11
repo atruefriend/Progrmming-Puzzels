9c8e0b59180a8db78b1f1e169abeca22eda26540
unknown <stylish.nikhil@gmail.com>
2023-10-11 22:45:25 +0530

    proj req

--- FindTheSum/Program.cs
+++ FindTheSum/Program.cs
@@ -3,6 +3,11 @@ using FindTheSum;
 
 //Console.WriteLine("Hello, World!");
 
+//Problem statement
+//From the given array find the pair for which the sum is euqal to given number.
+//If you found the pair return true else return false.
+//Arrays is unsorted
+
 //inputs
 int[] numbers = new int[] {1, 2, 4, 4};
 int sum = 8;
